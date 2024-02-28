6eb949f36146147310f968c0c5f88f5f3db86803
Notkero <Notkerga@ucm.es>
2024-02-28 17:44:27 +0100

    Mejora formulario base operaciones, falta funcionalidad

--- src/formularios/form_operaciones.py
+++ src/formularios/form_operaciones.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk, messagebox, Canvas, Entry, Text, Button, PhotoImage
+from tkinter import ttk, messagebox, Canvas, Entry, Text, Button, PhotoImage, Checkbutton, IntVar, Label
+from PIL import Image, ImageDraw, ImageTk
 from datetime import datetime
 import pandas as pd
 import sys
@@ -83,7 +84,18 @@ class FormularioOperaciones(tk.Toplevel):
         # Crear el ComboBox
         button_image_1 = PhotoImage(
         file="src/imagenes/assets/boton_mercado_operaciones.png")
-        
+
+        def borrar_texto(event):
+            text_box = event.widget
+            text_box.delete(0, tk.END)
+
+        def checkbox_clicked(check_var):
+            if check_var.get() == 1:
+                print("Checkbox activado")
+            else:
+                print("Checkbox desactivado")
+
+
         self.mercados_var = tk.StringVar(value=mercados)
         self.combo_mercados = ttk.Combobox(canvas, textvariable=self.mercados_var, values=mercados)
         self.combo_mercados.place(x=20.0, y=16.0, width=758, height=32.0)  # Ajusta el tamaño y la posición según sea necesario
@@ -118,6 +130,8 @@ class FormularioOperaciones(tk.Toplevel):
             height=258.0
         )
 
+
+
         entry_image_1 = PhotoImage(
             file="src/imagenes/assets/entry_fondo_operaciones.png")
         entry_bg_1 = canvas.create_image(
@@ -125,144 +139,37 @@ class FormularioOperaciones(tk.Toplevel):
             250.0,
             image=entry_image_1
         )
-        entry_1 = Entry(
-            canvas,
-            bd=0,
-            bg="#30a4b4",
-            fg="#FFFFFF",
-            highlightthickness=0
-        )
-        entry_1.place(
-            x=28.0,
-            y=237.0,
-            width=742.0,
-            height=24.0
-        )
 
-        entry_image_2 = PhotoImage(
-            file="src/imagenes/assets/entry_cartera_operaciones.png")
-        entry_bg_2 = canvas.create_image(
-            98.5,
-            250.0,
-            image=entry_image_2
-        )
-        entry_2 = Entry(
-            canvas,
-            bd=0,
-            bg="#30a4b4",
-            fg="#FFFFFF",
-            highlightthickness=0
-        )
-        entry_2.place(
-            x=28.0,
-            y=237.0,
-            width=141.0,
-            height=24.0
-        )
-
-        entry_image_3 = PhotoImage(
-            file="src/imagenes/assets/entry_gran_latente_operaciones.png")
-        entry_bg_3 = canvas.create_image(
-            266.5,
-            250.0,
-            image=entry_image_3
-        )
-        entry_3 = Entry(
-            canvas,
-            bd=0,
-            bg="#30a4b4",
-            fg="#FFFFFF",
-            highlightthickness=0
-        )
-        entry_3.place(
-            x=185.0,
-            y=237.0,
-            width=163.0,
-            height=24.0
-        )
-
-        entry_image_4 = PhotoImage(
-            file="src/imagenes/assets/entry_gran_dia_operaciones.png")
-        entry_bg_4 = canvas.create_image(
-            428.5,
-            250.0,
-            image=entry_image_4
-        )
-        entry_4 = Entry(
-            canvas,
-            bd=0,
-            bg="#30a4b4",
-            fg="#FFFFFF",
-            highlightthickness=0
-        )
-        entry_4.place(
-            x=364.0,
-            y=237.0,
-            width=129.0,
-            height=24.0
-        )
-
-        entry_image_5 = PhotoImage(
-            file="src/imagenes/assets/entry_ordenes_operaciones.png")
-        entry_bg_5 = canvas.create_image(
-            566.5,
-            250.0,
-            image=entry_image_5
-        )
-        entry_5 = Entry(
-            canvas,
-            bd=0,
-            bg="#30a4b4",
-            fg="#FFFFFF",
-            highlightthickness=0
-        )
-        entry_5.place(
-            x=509.0,
-            y=237.0,
-            width=115.0,
-            height=24.0
-        )
-
-        entry_image_6 = PhotoImage(
-            file="src/imagenes/assets/entry_gran_dia_operaciones.png")
-        entry_bg_6 = canvas.create_image(
-            705.0,
-            250.0,
-            image=entry_image_6
-        )
-        entry_6 = Entry(
-            canvas,
-            bd=0,
-            bg="#30a4b4",
-            fg="#FFFFFF",
-            highlightthickness=0
-        )
-        entry_6.place(
-            x=640.0,
-            y=237.0,
-            width=130.0,
-            height=24.0
-        )
+        canvas.create_text(28, 237, anchor="nw", text="Cartera: ", fill="white", font=("Calistoga Regular", 12))
+        canvas.create_text(185, 237, anchor="nw", text="Gan. latente: ", fill="white", font=("Calistoga Regular", 12))
+        canvas.create_text(364, 237, anchor="nw", text="Gan. día: ", fill="white", font=("Calistoga Regular", 12))
+        canvas.create_text(509, 237, anchor="nw", text="Órdenes: ", fill="white", font=("Calistoga Regular", 12))
+        canvas.create_text(640, 237, anchor="nw", text="Posición: ", fill="white", font=("Calistoga Regular", 12))
+        
 
         button_image_3 = PhotoImage(
-            file="src/imagenes/assets/boton_comun_acciones_tiempo.png")
-        button_3 = Button(
-            canvas,
-            image=button_image_3,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_3 clicked"),
-            relief="flat"
-        )
-        button_3.place(
-            x=20.0,
-            y=86.0,
-            width=151.0,
-            height=32.0
-        )
+        file="src/imagenes/assets/boton_comun_acciones_tiempo.png")
+
+        velas = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
+        self.velas_var = tk.StringVar(value=velas)
+        self.combo_velas = ttk.Combobox(canvas, textvariable=self.velas_var, values=velas)
+        self.combo_velas.place(x=20.0, y=86.0, width=151.0, height=32.0)  # Ajusta el tamaño y la posición según sea necesario
+        self.combo_velas.current(0)  # Establece la opción por defecto
+        self.combo_velas.configure(background='#30A4B4', foreground='#FFFFFF', font=('Calistoga Regular', 12))
+
+
+        # Función para manejar la selección en el ComboBox
+        def seleccionar_velas(event):
+            selected_item = self.velas_var.get()
+            print("Opción seleccionada:", selected_item)
+            self.combo_velas.configure(background='#30A4B4', foreground='#FFFFFF', font=('Calistoga Regular', 12))
+            # hay que hacer que se muestre la info del mercado seleccionado
+
+        self.combo_velas.bind("<<ComboboxSelected>>", seleccionar_velas)  # Asocia la función al evento de selección del ComboBox
+
 
         button_image_4 = PhotoImage(
-            file="src/imagenes/assets/boton_comun_lim_stop_key.png")
+            file="src/imagenes/assets/boton_lim.png")
         button_4 = Button(
             canvas,
             image=button_image_4,
@@ -280,47 +187,57 @@ class FormularioOperaciones(tk.Toplevel):
 
         button_image_5 = PhotoImage(
             file="src/imagenes/assets/boton_comun_lim_stop_key.png")
-        button_5 = Button(
-            canvas,
-            image=button_image_5,
-            borderwidth=0,
+        self.button_5 = Entry(
+            canvas,            
+            bd=0,
+            bg="#30a4b4",
+            fg="#FFFFFF",
+            font=('Calistoga Regular', 12),
             highlightthickness=0,
-            command=lambda: print("button_5 clicked"),
             relief="flat"
         )
-        button_5.place(
+
+        self.button_5.place(
             x=292.0,
             y=86.0,
             width=55.0,
             height=32.0
         )
+        texto_compras = "Nº compras"
+        self.button_5.insert(0, texto_compras)
+        self.button_5.bind("<FocusIn>", borrar_texto)
+
+
 
         button_image_6 = PhotoImage(
             file="src/imagenes/assets/boton_comun_mkt.png")
         button_6 = Button(
             canvas,
+            text="Precio compra",
             image=button_image_6,
             borderwidth=0,
             highlightthickness=0,
             command=lambda: print("button_6 clicked"),
-            relief="flat"
+            compound=tk.CENTER,
+            font=("Calistoga Regular", 12)
         )
-        button_6.place(
-            x=380.0,
-            y=86.0,
-            width=115.0,
-            height=32.0
-        )
+
+        button_window = canvas.create_window(380, 86, anchor='nw', window=button_6, width=115, height=32)
+        button_6.bind("<Button-1>", lambda event: seleccionar_velas())
+
+
 
         button_image_7 = PhotoImage(
             file="src/imagenes/assets/boton_cantidad_operaciones.png")
         button_7 = Button(
             canvas,
+            text="\uf0ad",
             image=button_image_7,
             borderwidth=0,
             highlightthickness=0,
             command=lambda: print("button_7 clicked"),
-            relief="flat"
+            compound=tk.CENTER,
+            font="font_awesome"
         )
         button_7.place(
             x=528.0,
@@ -329,25 +246,28 @@ class FormularioOperaciones(tk.Toplevel):
             height=32.0
         )
 
+
+
         button_image_8 = PhotoImage(
             file="src/imagenes/assets/boton_comun_mkt.png")
         button_8 = Button(
             canvas,
+            text="Precio venta",
             image=button_image_8,
             borderwidth=0,
             highlightthickness=0,
             command=lambda: print("button_8 clicked"),
-            relief="flat"
-        )
-        button_8.place(
-            x=380.0,
-            y=156.0,
-            width=115.0,
-            height=32.0
+            compound=tk.CENTER,
+            font=("Calistoga Regular", 12)
         )
 
+        button_window = canvas.create_window(380, 156, anchor='nw', window=button_8, width=115, height=32)
+        button_8.bind("<Button-1>", lambda event: seleccionar_velas())
+
+
+
         button_image_9 = PhotoImage(
-            file="src/imagenes/assets/boton_comun_lim_stop_key.png")
+            file="src/imagenes/assets/boton_stop.png")
         button_9 = Button(
             canvas,
             image=button_image_9,
@@ -365,12 +285,13 @@ class FormularioOperaciones(tk.Toplevel):
 
         button_image_10 = PhotoImage(
             file="src/imagenes/assets/boton_comun_lim_stop_key.png")
-        button_10 = Button(
-            canvas,
-            image=button_image_10,
-            borderwidth=0,
+        button_10 = Entry(
+            canvas,            
+            bd=0,
+            bg="#30a4b4",
+            fg="#FFFFFF",
+            font=('Calistoga Regular', 12),
             highlightthickness=0,
-            command=lambda: print("button_10 clicked"),
             relief="flat"
         )
         button_10.place(
@@ -379,101 +300,136 @@ class FormularioOperaciones(tk.Toplevel):
             width=55.0,
             height=32.0
         )
+        texto_ventas = "Nº ventas"
+        button_10.insert(0, texto_ventas)
+        button_10.bind("<FocusIn>", borrar_texto)
+
+
+
 
         button_image_11 = PhotoImage(
             file="src/imagenes/assets/boton_comun_numS_numO.png")
-        button_11 = Button(
-            canvas,
-            image=button_image_11,
-            borderwidth=0,
+        self.button_11 = Entry(
+            canvas,            
+            bd=0,
+            bg="#30a4b4",
+            fg="#FFFFFF",
+            font=('Calistoga Regular', 12),
             highlightthickness=0,
-            command=lambda: print("button_11 clicked"),
             relief="flat"
         )
-        button_11.place(
+
+        self.button_11.place(
             x=569.0,
             y=156.0,
             width=67.0,
             height=32.0
         )
+        texto_stop = "Stop"
+        self.button_11.insert(0, texto_stop)
+        self.button_11.bind("<FocusIn>", borrar_texto)
+
 
         button_image_12 = PhotoImage(
             file="src/imagenes/assets/boton_comun_numS_numO.png")
-        button_12 = Button(
-            canvas,
-            image=button_image_12,
-            borderwidth=0,
+        self.button_12 = Entry(
+            canvas,            
+            bd=0,
+            bg="#30a4b4",
+            fg="#FFFFFF",
+            font=('Calistoga Regular', 12),
             highlightthickness=0,
-            command=lambda: print("button_12 clicked"),
             relief="flat"
         )
-        button_12.place(
+
+        self.button_12.place(
             x=711.0,
             y=156.0,
             width=67.0,
             height=32.0
         )
+        texto_objetivo = "Objetivo"
+        self.button_12.insert(0, texto_objetivo)
+        self.button_12.bind("<FocusIn>", borrar_texto)
 
-        button_image_13 = PhotoImage(
-            file="src/imagenes/assets/boton_comun_S_O.png")
-        button_13 = Button(
+
+
+        check_S_var = IntVar()
+        checkbox_S = Checkbutton(
             canvas,
-            image=button_image_13,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_13 clicked"),
-            relief="flat"
+            text="S",  # Etiqueta del checkbox
+            variable=check_S_var,  # Variable de control para el estado del checkbox
+            onvalue=1,  # Valor cuando el checkbox está seleccionado
+            offvalue=0,  # Valor cuando el checkbox está deseleccionado
+            command=checkbox_clicked(check_S_var),  # Función para manejar los clics del checkbox
+            bd=0,  # Grosor del borde del checkbox
+            bg="white",  # Color de fondo del checkbox
+            activebackground="white",  # Color de fondo cuando el checkbox está activo
+            highlightthickness=0,  # Grosor del borde cuando el checkbox está resaltado
+            relief="flat"  # Tipo de relieve del checkbox
         )
-        button_13.place(
+        checkbox_S.place(
             x=528.0,
             y=156.0,
             width=28.0,
             height=32.0
         )
 
-        button_image_14 = PhotoImage(
-            file="src/imagenes/assets/boton_comun_S_O.png")
-        button_14 = Button(
+
+        check_O_var = IntVar()
+        checkbox_O = Checkbutton(
             canvas,
-            image=button_image_14,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_14 clicked"),
-            relief="flat"
+            text="O",  # Etiqueta del checkbox
+            variable=check_O_var,  # Variable de control para el estado del checkbox
+            onvalue=1,  # Valor cuando el checkbox está seleccionado
+            offvalue=0,  # Valor cuando el checkbox está deseleccionado
+            command=checkbox_clicked(check_O_var),  # Función para manejar los clics del checkbox
+            bd=0,  # Grosor del borde del checkbox
+            bg="white",  # Color de fondo del checkbox
+            activebackground="white",  # Color de fondo cuando el checkbox está activo
+            highlightthickness=0,  # Grosor del borde cuando el checkbox está resaltado
+            relief="flat"  # Tipo de relieve del checkbox
         )
-        button_14.place(
+        checkbox_O.place(
             x=669.0,
             y=156.0,
             width=28.0,
             height=32.0
         )
 
+
         button_image_15 = PhotoImage(
-            file="src/imagenes/assets/boton_comun_acciones_tiempo.png")
-        button_15 = Button(
-            canvas,
-            image=button_image_15,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_15 clicked"),
-            relief="flat"
-        )
-        button_15.place(
-            x=20.0,
-            y=156.0,
-            width=151.0,
-            height=32.0
-        )
+        file="src/imagenes/assets/boton_comun_acciones_tiempo.png")
+
+        frecuencia = ['1M', '2M', '5M', '10M', '15M', '20M', '30M', '1H', '2H', '3H', '4H', '6H', '8H', '12H', 'Daily', 'Weekly', 'Monthly']
+        self.frecuencia_var = tk.StringVar(value=frecuencia)
+        self.combo_frecuencia = ttk.Combobox(canvas, textvariable=self.frecuencia_var, values=frecuencia)
+        self.combo_frecuencia.place(x=20.0, y=156.0, width=151.0, height=32.0)  # Ajusta el tamaño y la posición según sea necesario
+        self.combo_frecuencia.current(0)  # Establece la opción por defecto
+        self.combo_frecuencia.configure(background='#30A4B4', foreground='#FFFFFF', font=('Calistoga Regular', 12))
+
+
+        # Función para manejar la selección en el ComboBox
+        def seleccionar_frecuencia(event):
+            selected_item = self.mercados_var.get()
+            print("Opción seleccionada:", selected_item)
+            self.combo_frecuencia.configure(background='#30A4B4', foreground='#FFFFFF', font=('Calistoga Regular', 12))
+            # hay que hacer que se muestre la info del mercado seleccionado
+
+        self.combo_frecuencia.bind("<<ComboboxSelected>>", seleccionar_frecuencia)  # Asocia la función al evento de selección del ComboBox
+
 
         button_image_16 = PhotoImage(
             file="src/imagenes/assets/boton_comun_lim_stop_key.png")
         button_16 = Button(
             canvas,
+            text="\uf0ad",
             image=button_image_16,
             borderwidth=0,
             highlightthickness=0,
             command=lambda: print("button_16 clicked"),
-            relief="flat"
+            compound=tk.CENTER,
+            font="font_awesome"
         )
         button_16.place(
             x=721.0,
