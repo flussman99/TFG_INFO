72b59923eb02cb1112c4fdfbd2f7c3a2f81ababb
david <davidvie@ucm.es>
2024-05-27 20:08:12 +0200

    quitar comenatrios

--- src/EquiposdeFutbol/SBS_backtesting.py
+++ src/EquiposdeFutbol/SBS_backtesting.py
@@ -1,5 +1,3 @@
-                                                                    #SOLO LOS PARTIDOS DE LIGA
-
 
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -10,7 +8,6 @@ import tick_reader as tr
 import MetaTrader5 as mt5
 from typing import Tuple
 from config import API_KEY 
-import schedule
 import time
 from datetime import datetime, time, timedelta
 
@@ -43,7 +40,6 @@ pais = {
         'ACS': 'spain',
         'ADS': 'united states',
         'NKE': 'united states',
-        # 'SPOT': 'united states',
         'DTEGn': 'germany',
         'ALVG': 'germany',
         'KO' : 'united states',
@@ -202,7 +198,7 @@ def backtesting(ticks: list,inicio: str, fin: str,url,combo_comprar:str,combo_ve
     equipos_frame=datosEquipos(ticks,inicio, fin,url,equipos_txt) 
     # Initialize a new column 'precio' in equipos_frame with NaN values
     
-    # equipos_frame['Fecha'] = ticks_frame['Fecha'].dt.date#solo la fecha no la hora
+    
     posicion_abierta=False #comprobar si hay alguna posicion abierta para poder realziar ventas 
     decisiones = []
     rentabilidad=[]
@@ -224,7 +220,7 @@ def backtesting(ticks: list,inicio: str, fin: str,url,combo_comprar:str,combo_ve
             compras.append(precioCompra)
             posicion_abierta=True
         else:
-            decisiones.append("NO SE REALIZA OPERACION")#COMPRO
+            decisiones.append("NO SE REALIZA OPERACION")
             rentabilidad.append(None)
     compras.clear()
     # Agregar la lista de decisiones como una nueva columna al DataFrame
@@ -309,7 +305,6 @@ def ultimoPartido(equipos_txt:str,url,cola):
 
     leerUrl(url)#cojo los partidos
     equipos_frame = pd.DataFrame(data, columns=['Fecha', 'Competición', 'Equipo Local', 'Equipo Visitante','Marcador', 'ResultadoLocal', 'ResultadoVisitante'])
-    # equipos_frame['Fecha'] = pd.to_datetime(equipos_frame['Fecha'])
     data.clear()#ya lo tengo que limpiar
     last_row = equipos_frame.iloc[-1]
     if(FECHA_ULTIMO_PARTIDO is None or last_row['Fecha']!=FECHA_ULTIMO_PARTIDO):
@@ -813,84 +808,3 @@ def convert_date(date_str):
     month = month_names[month.upper()]
     return f"{year}-{month}-{day.zfill(2)}"
 
-
-
-    # if CUR_SIGNAL.iloc[-1] <= CUR_MACD.iloc[-1] and CUR_RSI.iloc[-1] > 65:
-    #     return True
-    # return False
-
-
-
-                                                                        # #TODOS LOS PARTIDOS
-
-    # import pandas as pd
-    # from bs4 import BeautifulSoup
-
-    # # Lista de archivos HTML a procesar
-    # html_files = [
-    #     'html/2014-2015.html',
-    #     'html/2015-2016.html',
-    #     'html/2016-2017.html',
-    #     'html/2017-2018.html',
-    #     'html/2018-2019.html',
-    #     'html/2019-2020.html',
-    #     'html/2020-2021.html',
-    #     'html/2021-2022.html',
-    #     'html/2022-2023.html'
-    # ]
-
-    # # Lista para almacenar los datos de todos los archivos
-    # data = []
-
-    # for file_name in html_files:
-    #     with open(file_name, 'r', encoding='utf-8') as file:
-    #         html_content = file.read()
-    #         soup = BeautifulSoup(html_content, 'html.parser')
-    #         matches = soup.find_all('a', class_='match-link', attrs={'data-cy': 'match'})
-            
-    #         for match in matches:
-    #                 competition_elem = match.find('div', class_='middle-info')
-    #                 date_elem = match.find('div', class_='date-transform')
-    #                 home_team_elem = match.find('div', class_='team-name', itemprop='name')
-    #                 away_team_elems = match.find_all('div', class_='team-name', itemprop='name')
-    #                 result_elem = match.find('div', class_='marker')
-                    
-    #                 if date_elem and home_team_elem and len(away_team_elems) > 1 and result_elem:
-    #                     result_spans = result_elem.find_all('span')
-    #                     if len(result_spans) == 3:  # Ensure there are two result spans and one dash
-    #                         result_local = result_spans[1].text.strip()
-    #                         result_visitante = result_spans[2].text.strip()
-    #                         data.append([
-    #                             date_elem.text.strip(),
-    #                             competition_elem.text.strip(),
-    #                             home_team_elem.text.strip(),
-    #                             away_team_elems[1].text.strip(),
-    #                             result_elem.text.strip(),
-    #                             result_local,
-    #                             result_visitante
-    #                         ])
-
-    # # Crear un DataFrame con los datos combinados
-    # df = pd.DataFrame(data, columns=['Fecha', 'Competición', 'Equipo Local', 'Equipo Visitante','Resultado', 'ResultadoLocal', 'ResultadoVisitante'])
-
-    # # Después de crear el DataFrame
-    # for index, row in df.iterrows():
-    #     if row['Equipo Local'] == 'Real Madrid':
-    #         if int(row['ResultadoLocal']) > int(row['ResultadoVisitante']):
-    #             df.loc[index, 'Decision'] = 'Ganado'
-    #         elif int(row['ResultadoLocal']) < int(row['ResultadoVisitante']):
-    #             df.loc[index, 'Decision'] = 'Perdido'
-    #         else:
-    #             df.loc[index, 'Decision'] = 'Empatado'
-    #     else:
-    #         if int(row['ResultadoVisitante']) > int(row['ResultadoLocal']):
-    #             df.loc[index, 'Decision'] = 'Ganado'
-    #         elif int(row['ResultadoLocal']) > int(row['ResultadoVisitante']):
-    #             df.loc[index, 'Decision'] = 'Perdido'
-    #         else:
-    #             df.loc[index, 'Decision'] = 'Empatado'
-
-
-
-    # # Guardar los datos en un archivo Excel
-    # df.to_excel('real_madrid_matches_combined.xlsx', index=False)
